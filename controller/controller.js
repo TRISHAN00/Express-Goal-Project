11158710d658bd35316e1e4052b2d2caeca3b579
TRISHAN00 <trishansaha43@gmail.com>
2022-10-21 02:06:01 +0600

    made shortcut of router

--- controller/controller.js
+++ controller/controller.js
@@ -1,15 +1,28 @@
+// @desc Get Goals
+// @route GET /api/goals
+// @access Private
+
 const getGoals = (req, res) => {
-  res.status(200).json({ message: "Get Goals on PORT" });
+  res.status(200).json({ message: "Get Goals" });
 };
 
+// @desc Set Goals
+// @route POST /api/goals
+// @access Private
 const setGoal = (req, res) => {
   res.status(200).json({ message: "Set Goal" });
 };
 
+// @desc Update Goals
+// @route PUT /api/goals
+// @access Private
 const updateGoal = (req, res) => {
   res.status(200).json({ message: `Update goal on ${req.params.id}` });
 };
 
+// @desc Delete Goals
+// @route DELETE /api/goals
+// @access Private
 const deleteGoal = (req, res) => {
   res.status(200).json({ message: `Delete goal on ${req.params.id}` });
 };
