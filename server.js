f9ad0ef1c6e25f824876216d0e5ca6e0c383cfe2
TRISHAN00 <trishansaha43@gmail.com>
2022-10-21 01:23:53 +0600

    left node modules and .env

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("This is goal routes");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);
