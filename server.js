6926361b61c03cc8c470258b3a7326c931ede41e
TRISHAN00 <trishansaha43@gmail.com>
2022-10-21 15:58:16 +0600

    see requiest body

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/routes.js"));
 
 const PORT = process.env.PORT || 5000;
