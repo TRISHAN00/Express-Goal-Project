df2616d01cd6487f980837a97ce44525e6634683
TRISHAN00 <trishansaha43@gmail.com>
2022-10-21 16:02:23 +0600

    error handler

--- controller/controller.js
+++ controller/controller.js
@@ -10,7 +10,9 @@ const getGoals = (req, res) => {
 // @route POST /api/goals
 // @access Private
 const setGoal = (req, res) => {
-  console.log(req.body);
+  if (!req.body.text) {
+    res.status(400).json({ message: "Please add a text field." });
+  }
   res.status(200).json({ message: "Set Goal" });
 };
 
